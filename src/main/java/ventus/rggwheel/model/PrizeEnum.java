72550df1ede4545603587b3e078e34817bfe0001
ventus34 <ventus34forspam@gmail.com>
2024-05-16 20:28:13 +0200

    Minor layout fixes

--- src/main/java/ventus/rggwheel/model/PrizeEnum.java
+++ src/main/java/ventus/rggwheel/model/PrizeEnum.java
@@ -6,13 +6,13 @@ public enum PrizeEnum {
     LUCKY_SUB("Lucky Sub", "The last subscriber/member chooses next game on the current platform.", "Lucky Sub"),
     CHAT_PICK("Chat of Five", "Viewers can vote for 1 of the 5 games displayed by the generator.", "Chat o'Five"),
     JUAN_OF_FIVE("Juan of Five", "The player rolls next game in the generator and can pick one from the 5 games shown on screen.", "Juan o'Five"),
-    SPINNER_OF_FIVE("Spinner of Five", "Next game will be selected by the supporter among the 5 games displayed by the generator on the current platform.", "Spinner o’ Five"),
+    SPINNER_OF_FIVE("Spinner of Five", "Next game will be selected by the supporter among the 5 games displayed by the generator on the current platform.", "Spinner o'Five"),
     SPIN_AGAIN("Spin again", "Player spins the wheel one more time.", "Spin again"),
     TIER_PLUS("Tier plus", "Donation and Sub incentives amounts increase by 5.", "Tier +"),
-    AUCTION_GAME("Auction Game", "A 3-minute timer will be started. The person who donated the largest single donation chooses next game on the current platform.", "Auction Game"),
-    COIN_FLIP("Double or Nothing", "Player can bet any amount of coins on a coin flip! Good luck!", "Coin flip"),
-    BAG("Mysterious Bag", "A random number of coins is added/substracted from -30 to +50", "Mysterious Bag"),
-    HOT_STREAK("Hot Streak", "Coins are awarded to the player for his current completed games streak at +5 coins per game. Use !rgg for more info", "Heads/Tails"),
+    AUCTION_GAME("Auction", "A 3-minute timer will be started. The person who donated the largest single donation chooses next game on the current platform.", "Auction"),
+    COIN_FLIP("Coin flip", "Player can bet any amount of coins on a coin flip! Good luck!", "Coin flip"),
+    BAG("Mystery Bag", "A random number of coins is added/substracted from -30 to +50", "Mystery Bag"),
+    HOT_STREAK("Hot Streak", "Coins are awarded to the player for his current completed games streak at +5 coins per game. Use !rgg for more info", "Hot Streak"),
     CURSE("Curse", "What a horrible night to have a “cursed wheel”, the player spins again the wheel but with spooky effects. Use !rgg for spooky effects table", "Curse"),
     RESET("Reset", "Subs and Donation incentives for a given platform resets (this includes current donations and subs points). Use !rgg for more info", "Reset");
 
